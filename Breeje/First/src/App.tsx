c96667c64e9265875406d1a0096061657b7f994e
Nati-darse <natnaeldarsema@gmail.com>
2025-09-26 21:10:27 +0300

    main files for the projects

---
+++ Breeje/First/src/App.tsx
@@ -0,0 +1,27 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import TrustedBy from "./components/TrustedBy";
+import Features from "./components/Features";
+import AISection from "./components/AISection";
+import Testimonials from "./components/Testimonials";
+import FAQ from "./components/FAQ";
+import Articles from "./components/Articles";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="bg-[#0a0118] text-white">
+      <Navbar />
+      <Hero />
+      <TrustedBy />
+      <Features />
+      <AISection />
+      <Testimonials />
+      <FAQ />
+      <Articles />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
