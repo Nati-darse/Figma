ff9737daff8bcb54231f6500f8bac508b686ffe8
Nati-darse <natnaeldarsema@gmail.com>
2025-09-30 23:45:31 +0300

    format of listed cards updated

--- Breeje/First/src/components/Features.tsx
+++ Breeje/First/src/components/Features.tsx
@@ -1,5 +1,7 @@
 import SectionTitle from "./UI/SectionTitle";
 import Card from "./UI/Card";
+import Button from "./UI/Button";
+import { FaCheckCircle } from "react-icons/fa";
 
 export default function Features() {
   const features = [
@@ -11,26 +13,36 @@ export default function Features() {
   ];
 
   return (
-    <section className="py-20 px-6 text-center">
+    <section className="bg-[#01051e] py-20 px-6 text-center">
       <SectionTitle title="Explore Algen’s Powerful Features" />
+      <div className="mt-8 flex items-center justify-center gap-4">
+        
+       <Button>Get started</Button>
+      </div>
       <div className="mt-12 grid md:grid-cols-2 gap-8 max-w-5xl mx-auto">
+        <div className="bg-red-200/50 rounded-lg pt-2 m-0 ">
         <Card title="Dominate Multiple Marketplaces">
-          <p className="text-gray-400 text-sm">
+          <p className="text-gray-400 text-sm py-0 my-0">
             Unlock AI-driven analytics across multiple eCommerce platforms with
             one dashboard.
           </p>
         </Card>
+        </div>
+        <div className="bg-red-800/50 rounded-lg p-5 ">
         <Card title="Select a marketplace to see trends">
           <div className="bg-gray-800/50 rounded-lg p-4 mt-4">
             <p className="text-gray-400 text-sm">[Dropdown of Marketplaces]</p>
           </div>
         </Card>
-      </div>
-
-      <div className="mt-12 grid md:grid-cols-3 gap-6">
-        {features.map((feature, idx) => (
-          <Card key={idx} title={feature} />
-        ))}
+        </div>
+        <ul className="space-y-4 ml-8 text-left">
+            {features.map((feature, idx) => (
+              <li key={idx} className="flex items-center gap-3">
+                <FaCheckCircle className="text-purple-500" />
+                <span className="text-gray-300">{feature}</span>
+              </li>
+            ))}
+          </ul>
       </div>
     </section>
   );
