9955243eccfa58ab8187a77ab3c8797d3ead0305
Nati-darse <natnaeldarsema@gmail.com>
2025-09-30 23:44:48 +0300

    update the card placement

--- Breeje/First/src/components/UI/Card.tsx
+++ Breeje/First/src/components/UI/Card.tsx
@@ -7,7 +7,7 @@ type CardProps = {
 
 export default function Card({ title, children }: CardProps) {
   return (
-    <div className="bg-gray-800/40 p-6 rounded-lg border border-gray-700/40 text-left">
+    <div className="bg-gray-800/40 p-2 rounded-lg border border-gray-700/40 text-left">
       <h3 className="text-lg font-semibold mb-3 text-white">{title}</h3>
       {children}
     </div>
