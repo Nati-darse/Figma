2552408bc6d1afd9b5fceb724505d6f833b1c090
Nati-darse <natnaeldarsema@gmail.com>
2025-10-02 07:57:08 +0300

    build frequently added question section for the page

--- Breeje/First/src/components/FAQ.tsx
+++ Breeje/First/src/components/FAQ.tsx
@@ -2,13 +2,13 @@ import SectionTitle from "./UI/SectionTitle";
 
 export default function FAQ() {
   const faqs = [
-    { q: "What can Algen help me with?", a: "Algen helps analyze sales data across marketplaces." },
+    { q: "What can Algen help me with?", a: "Algen helps analyze sales data across marketplaces. Algen helps analyze sales data across marketplaces Algen helps analyze sales data across marketplaces" },
     { q: "Is there a free trial?", a: "Yes, you can start with a free plan." },
     { q: "Which marketplaces are supported?", a: "Amazon, Walmart, Shopify, eBay, and more." },
   ];
 
   return (
-    <section className="py-20 px-6 max-w-4xl mx-auto">
+    <section className="py-20 bg-[#01051e] px-48 max-w-1xl mx-auto">
       <SectionTitle title="Frequently Asked Questions" />
       <div className="mt-12 space-y-6">
         {faqs.map((faq, idx) => (
