173e4e30a9ba91af05af516aad53eb7122ed0fa4
Nati-darse <natnaeldarsema@gmail.com>
2025-11-03 03:02:46 +0300

    offer

---
+++ unk/I/src/app/offer/offer.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+interface Offer {
+  title: string;
+  image: string;
+}
+
+@Component({
+  selector: 'app-offer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './offer.html',
+  styleUrls: ['./offer.css']
+})
+export class OfferComponent {
+  offers: Offer[] = [
+    { title: 'Complete Project Management', image: '/assets/offer-1.jpg' },
+    { title: 'High-Quality Materials', image: '/assets/offer-2.jpg' },
+    { title: 'On-Time Delivery', image: '/assets/offer-3.jpg' },
+    { title: 'Interiors Items', image: '/assets/offer-4.jpg' }
+  ];
+}
