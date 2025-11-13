028b19d7ef69fb19bac1ecd6543520d5e56832b6
Nati-darse <natnaeldarsema@gmail.com>
2025-11-13 23:46:32 +0300

    footer section of the page has been structured

--- unk/I/src/app/Components/footer/footer.component.ts
+++ unk/I/src/app/Components/footer/footer.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-footer',
-    standalone: true,
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
